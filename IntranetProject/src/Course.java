2f191b431380b8bde91eaa648f42ef6cdc99b6ee
Алдияр Есентуров <aldie1741@gmail.com>
2019-11-18 15:00:52 +0600

    Course finished

--- IntranetProject/src/Course.java
+++ IntranetProject/src/Course.java
@@ -1,9 +1,9 @@
 import java.util.*;
 public class Course {
-	private	Course Prereq,Prereq1,Prereq2;
+	private	Course Prereq;
 	private String name,id;
 	boolean isPrereq;
-	Stack<Course> Pre = new Stack();
+	Vector<Course> Pre = new Vector<Course>();
 	Course(){}
 	Course(String name, String id){
 		this.name=name;
@@ -12,10 +12,14 @@ public class Course {
 	public void addPrereq(Course a) {
 		if(Pre.size()<3)
 			Pre.add(a);
-		else System.out.println("");
-	}
+		else {System.out.println("Error!! Course reached upper limit of prerequisites!!");
+			
+		}}
 	public void showPrereq() {
-		return Prereq;	
+		System.out.println("Course Prerequisites: ");
+		for(Course q:Pre) {
+		System.out.println(q.toString() + " ");
+		}	
 	}
 	public String getName(){
 		return name;
@@ -29,13 +33,40 @@ public class Course {
 	public void setId(String id) {
 		this.id=id;
 	}
-	public void removePrereq() {
-		
+	public void removePrereq(String id) {
+		for(Course q:Pre) { if(q.id.equals(id)) {
+			Pre.remove(q);
+			System.out.println("Delete completed." + "");
+		}
+			
+			}	
 	}
+	public void removePrereq(Course i) {
+		for(Course q:Pre) { if(q.equals(i)) {
+			Pre.remove(q);
+			System.out.println("Delete completed." + "");
+		}}
+			
+			}	
+	
 
-public native int hashCode();
-	public boolean equals(Object obj) {
-        return (this == obj);
-}
+	@Override
+	public String toString() {
+		return "Course [name=" + name + ", id=" + id + "]";
 	}
+	public int hashCode() {
+		 int result = 17;
+	        result = 31 * result + name.hashCode();
+	        result = 31 * result + id.hashCode();
+	        return result;
+	};
+
+	public boolean equals(Course obj) {
+        return (this.name.equals(obj.name) && this.id.equals(obj.id) );
+        	
+        }
+     
 }
+	
+	 
+
