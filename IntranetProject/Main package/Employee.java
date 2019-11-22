2e6b8b8f9f67def390456f9af5d1d78efeffcaac
Алдияр Есентуров <aldie1741@gmail.com>
2019-11-22 13:39:31 +0600

    Abstracts

---
+++ IntranetProject/Main package/Employee.java
@@ -0,0 +1,13 @@
+
+public class Employee {
+int salary=0;
+
+public int getSalary() {
+	return salary;
+}
+
+public void setSalary(int salary) {
+	this.salary = salary;
+}
+
+}
---
+++ IntranetProject/Main package/User.java
@@ -0,0 +1,45 @@
+
+public abstract class User {
+String name,surname,id,login,password;
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getSurname() {
+	return surname;
+}
+
+public void setSurname(String surname) {
+	this.surname = surname;
+}
+
+public String getId() {
+	return id;
+}
+
+public void setId(String id) {
+	this.id = id;
+}
+
+public String getLogin() {
+	return login;
+}
+
+public void setLogin(String login) {
+	this.login = login;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+}
+
+}
