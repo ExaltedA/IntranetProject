9032c3bf0c73250daadcf3692d423d428e017457
Алдияр Есентуров <aldie1741@gmail.com>
2019-12-03 16:10:15 +0600

    keys and Core modified

--- IntranetProject/Main package/Core.java
+++ IntranetProject/Main package/Core.java
@@ -1,13 +1,14 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.*;
+import java.nio.channels.FileChannel;
 
 public class Core {
+	User curSession;
 	Databases data = Databases.getInstance();
 	static int maxAttempt = 0;
 	Core(){
 
-		data.students.add(new Student("Daniyar", "Moldakhan", "18BD112356", "d_moldakhan", "Kbtu111", "FIT", "CSS", 2, 3, 21, StudentType.BACHELOR));
+		data.students.add(new Student("Daniyar", "Moldakhan", "18BD112356", "a_esenturov", "Aldik12", "FIT", "CSS", 2, 3, 21, StudentType.BACHELOR));
 		data.students.add(new Student("Assel", "Bekbayeva", "17BD120125", "a_bekbayeva", "Novembar2019", "FOGI", "FOGI" , 3, 3.5, 19,StudentType.BACHELOR));
 		data.students.add(new Student("Adil", "Seitzhanov", "19BD32208", "a_seitzhanov", "August2019", "SCE", "CTOS", 1, 2.8, 19,StudentType.BACHELOR));
 		data.students.add(new Student("Zhanna", "Kissenova", "18BD240613", "zh_kissenova", "Astana2017", "BS", "Finance" , 2, 3.2, 21,StudentType.BACHELOR));
@@ -17,8 +18,17 @@ public class Core {
 		System.out.println("Welcome to the KBTU Intranet!");
 		System.out.println("Please Enter your login and Password: ");
 		
-		processSignIn();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		
+		
+		processSignIn();
+		showNews();
+		String any = br.readLine();
+		if(!any.isEmpty()) {
+			
+		}
+
 	}
 	public void processSignIn() throws IOException { // Login and Password function
 		maxAttempt++;
@@ -33,19 +43,49 @@ public class Core {
 		for(int i = 0; i < data.students.size(); i++) {
 
 			if(login.equals(data.students.get(i).getLogin()) && password.equals(data.students.get(i).getPassword())) {
+				curSession = data.students.get(i);
 				System.out.println("Welcome!");
 				found= true;
 				break;
 			}    	
 
 		}
-		if(maxAttempt<2) {
+		if(maxAttempt<5) {
 			if(!found) {
 				System.out.println("Try again");
 				processSignIn();
 			}}
-			else System.out.println("Your entry data is invalid, please seek help from the Techguy or Admin, Telegram: @ExaltedA");
+		else System.out.println("Your entry data is invalid, please seek help from the Techguy or Admin, Telegram: @ExaltedA");
+	}
+	public void showNews() throws IOException {  //Mozhet dokapatsya https://stackoverflow.com/questions/3886201/java-outputting-text-file-to-console
+		FileInputStream input = new FileInputStream("C:/Users/aldie/Documents/Intranet/IntranetProject/bin/news.txt");
+		FileChannel channel = input.getChannel();
+		byte[] buffer = new byte[256 * 1024];
+		ByteBuffer byteBuffer = ByteBuffer.wrap(buffer);
+
+		try {
+		    for (int length = 0; (length = channel.read(byteBuffer)) != -1;) {
+		        System.out.write(buffer, 0, length);
+		        byteBuffer.clear();
+		    }
+		} finally {
+		    input.close();
+		}
+		System.out.println("Press any key to proceed to menu..");
+	}
+	
+	public void studentMenu() throws IOException {
+		System.out.println("");
+		
+		
+		Keys keys;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String command= br.readLine();
+		command.toUpperCase();
+		if(command.equals("1")) {
+		
+		}
 		
 	}
-
+	
 }
